47a63a032424272a6b13588f80791f0c3daef922
Gitlab CI <fabio.filasieno@gmail.com>
2025-08-04 22:46:34 +0200

    added delete test file

--- src/test_file_io.cc
+++ src/test_file_io.cc
@@ -4,16 +4,19 @@
 
 DefineTask MainTask() noexcept {
 	int res;
-	int fd = co_await XOpen("test.txt", O_RDWR | O_CREAT | O_TRUNC | O_NONBLOCK, 0666);
+
+	const char* path = "test.txt";
+	int fd = co_await XOpen(path, O_RDWR | O_CREAT | O_TRUNC | O_NONBLOCK, 0666);
 	std::print("Open res: {}\n", fd);
 	res = co_await XWrite(fd, "hello world!\n", 13, 0);
 	std::print("written: {}\n", res);
 	res = co_await XClose(fd);
 	std::print("Close res: {}\n", res);
+	res = co_await XUnlink(path, 0);
+	std::print("Unlink res: {}\n", res);
 	
   	co_return;
 }
-
 
 int main() {
 	KernelConfig config = {
