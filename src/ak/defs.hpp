ba4ff83e9ff87fe46ec2a7c4d2b88d3026eecb07
Gitlab CI <fabio.filasieno@gmail.com>
2025-08-16 17:49:59 +0200

    simplify bit field search

--- src/ak/defs.hpp
+++ src/ak/defs.hpp
@@ -1,10 +1,27 @@
 #pragma once
 
 namespace ak { 
-    
+    using Void = void;
+    using Bool = bool;
+    using Char = char;
+    using WChar = wchar_t;
+
     using U64  = unsigned long long;  
     using U32  = unsigned long; 
+    using U16  = unsigned short;
+    using U8   = unsigned char;
+
+    using I64  = signed long long;  
+    using I32  = signed long; 
+    using I16  = signed short;
+    using I8   = signed char;
+    
     using Size = unsigned long long; 
+    using ISize = signed long long; 
+    using PtrDiff = signed long long;
+
+    using F32 = float;
+    using F64 = double;    
 
     namespace priv {
 
