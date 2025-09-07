9637ec0e0dd995ac35609d43adedd33b9df942dd
Gitlab CI <fabio.filasieno@gmail.com>
2025-09-07 22:17:21 +0200

    update deps

--- lspd-plugin/src/extension.ts
+++ lspd-plugin/src/extension.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode';
 
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log("lspd plugin activated")
-    
     const disposable = vscode.commands.registerCommand('lspd.helloWorld', () => {
         vscode.window.showInformationMessage('Hello World from lspd!');
     });
